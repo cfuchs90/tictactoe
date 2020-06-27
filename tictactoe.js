af78fca0b0a2cd0a9318a77cb75ba0048fdef607
Christian Fuchs <fuchs.christian90@gmail.com>
2020-06-27 13:44:35 +0200

    updating board works

--- tictactoe.js
+++ tictactoe.js
@@ -5,11 +5,7 @@ const Player = function(playerName, playerSymbol) {
   const getName = () => name;
   const getSymbol = () => symbol;
 
-  const markField = function(event) {
-    event.target.textContent = this.getSymbol();
-  }
-
-  return {getName, getSymbol, markField};
+  return {getName, getSymbol};
 }
 
 
@@ -18,18 +14,16 @@ const Board = (function() {
   field = ["", "", "", "", "", "", "", "", ""];
 
   const print = () => console.log(field);
+  const update = () => field = Display.getBoard();
 
-  // const clear = () => field = field.map(item => item = "");
-
-  // return {field, print, clear};
-  return {field, print};
+  return {field, print, update};
 })()
 
 
 const Display = (function() {
   const grid = document.querySelectorAll('.player-mark')
 
-  const updateBoard = function() {
+  const getBoard = function() {
     let gridArray = Array.from(grid, x => x.textContent);
 
     for(let i = 0; i <= gridArray.length; i++)  {
@@ -46,14 +40,9 @@ const Display = (function() {
        a callback */
 
     const select = function(event) {
-      if(event.target.textContent === "") {
-	event.target.textContent = player.getSymbol();
-      } else {
-	// TODO: keep player in loop until correct choice was made, give better errors
-	alert("Heeeeell No!");
-      }
+      event.target.textContent = player.getSymbol();
       grid.forEach(item => item.removeEventListener("click", select));
-      updateBoard();
+      // updateBoard();
     }
 
     grid.forEach(item => item.addEventListener("click", select));
@@ -68,12 +57,12 @@ const Display = (function() {
   // }
 
   
-  return {grid, getSelection, updateBoard};
+  return {grid, getSelection, getBoard};
 })()
 
 
 
-const clearButton = document.querySelector('#clear');
+// const clearButton = document.querySelector('#clear');
 
 const player1 = Player("christian", "X");
 const player2 = Player("christian", "O");
@@ -81,25 +70,4 @@ let currentPlayer = player1
 
 // clearButton.addEventListener("click", () => Display.clear());
 
-
-// if(currentPlayer == player1) {
-//   Display.getInput(currentPlayer);
-//   currentPlayer = player2;
-// } else {
-//   Display.getInput(currentPlayer);
-//   currentPlayer = player1;
-// }
-
-		// Display.releaseGrid(player1);
-
-
-let counter = 0;
-
-// while(counter <= 9) {
-//   Display.getInput(player1);
-//   Display.updateBoard();
-//   Display.releaseGrid();
-//   Display.getInput(player2);
-//   Display.updateBoard();
-//   counter++;
-// }
+Display.getSelection(player1);
