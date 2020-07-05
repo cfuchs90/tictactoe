ec5289ba3edae066122827ee463c3126dbdbb293
Christian Fuchs <fuchs.christian90@gmail.com>
2020-07-05 21:58:37 +0200

    winning logic halfway implemented

--- tictactoe.js
+++ tictactoe.js
@@ -11,9 +11,17 @@ const Board = (function() {
   let field = new Array(9);
   field = ["", "", "", "", "", "", "", "", ""];
 
+  const winningConfigs = [ [field[0] == field[1] && field[1] == field[2]],
+			   [field[3] == field[4] && field[4] == field[5]],
+			   [field[6] == field[7] && field[7] == field[8]],
+			   [field[0] == field[3] && field[3] == field[6]],
+			   [field[0] == field[4] && field[4] == field[8]],
+			   [field[1] == field[4] && field[4] == field[7]],
+			   [field[2] == field[5] && field[5] == field[8]]];
+
   // const update = () => field = Display.getBoard();
 
-  return {field};
+  return {field, winningConfigs};
 })()
 
 
@@ -55,18 +63,26 @@ const Game = function(player1, player2) {
       if(currentPlayer == player1) {
 	if(event.target.textContent !== player2.symbol) {
 		event.target.textContent = player1.symbol;
+		Display.getBoard();
+		over();
 		currentPlayer = player2;
 	}
       } else {
 	 if(event.target.textContent !== player1.symbol) {
 	   event.target.textContent = player2.symbol;
+	    Display.getBoard();
+	    over();
 	   currentPlayer = player1;
 	}
       }
-      Display.getBoard();
+      // Display.getBoard();
+      // over();
   }
 
   const over = function() {
+    if(Board.field.slice(0,3).every(elem => elem == currentPlayer.symbol)) {
+      alert(`${currentPlayer.name} wins!`);
+  }
   }
 
   return {turn}
