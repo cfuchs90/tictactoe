2e2e12f2099d4370f9f7159410c36a2db3537544
Christian Fuchs <fuchs.christian90@gmail.com>
2020-10-21 12:23:51 +0200

    scoreboard works

--- tictactoe.js
+++ tictactoe.js
@@ -20,11 +20,10 @@ const Board = (function() {
 
 const Display = (function() {
   const grid = document.querySelectorAll('.player-mark')
-  const player1 = Player("christian", "X", true);
-  const player2 = Player("heinz", "O", false);
+  const scoreP1 = document.querySelector("#p1score")
+  const scoreP2 = document.querySelector("#p2score")
 
-  let currentPlayer = player1;
-
+  
   const getBoard = function() {
     let gridArray = Array.from(grid, x => x.textContent);
 
@@ -36,6 +35,7 @@ const Display = (function() {
     grid.forEach(element => element.removeEventListener("click", func));
     grid.forEach(element => element.addEventListener("click", func));
   }
+
   
   const clear = function() {
     grid.forEach((item) => {
@@ -44,8 +44,17 @@ const Display = (function() {
     Board.clear();
   }
 
-    
-  return { grid, getBoard, round, clear};
+  const score = function(winner) {
+    if(winner == player1.name) {
+      let scoreString = parseInt(scoreP1.textContent) +1;
+      scoreP1.textContent = scoreString;
+    } else {
+      let scoreString = parseInt(scoreP2.textContent) +1;
+      scoreP2.textContent = scoreString;
+    }
+  }
+
+  return { grid, getBoard, round, clear, score};
 })()
 
 
@@ -58,62 +67,72 @@ const Game = function(player1, player2) {
 	if(event.target.textContent !== player2.symbol) {
 		event.target.textContent = player1.symbol;
 		Display.getBoard();
-		win();
+		winner = win();
 		currentPlayer = player2;
 	}
       } else {
 	 if(event.target.textContent !== player1.symbol) {
 	   event.target.textContent = player2.symbol;
 	    Display.getBoard();
-	    win();
+	    winner = win();
 	   currentPlayer = player1;
 	}
       }
+
   }
 
   const win = function() {
     if([Board.field[0], Board.field[1], Board.field[2]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[0], Display.grid[1], Display.grid[2]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[0], Board.field[3], Board.field[6]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[0], Display.grid[3], Display.grid[6]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[1], Board.field[4], Board.field[7]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[1], Display.grid[4], Display.grid[7]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[2], Board.field[5], Board.field[8]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[2], Display.grid[5], Display.grid[8]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[2], Board.field[4], Board.field[6]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[2], Display.grid[4], Display.grid[6]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[0], Board.field[1], Board.field[2]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[0], Display.grid[1], Display.grid[2]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[3], Board.field[4], Board.field[5]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[3], Display.grid[4], Display.grid[5]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[6], Board.field[7], Board.field[8]].every(elem => elem == currentPlayer.symbol)) {
-      [Display.grid[6], Display.grid[7], Display.grid[8]].forEach(item => item.style.color = "red");w
+      [Display.grid[6], Display.grid[7], Display.grid[8]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
     } else if([Board.field[0], Board.field[4], Board.field[8]].every(elem => elem == currentPlayer.symbol)) {
       [Display.grid[0], Display.grid[4], Display.grid[8]].forEach(item => item.style.color = "red");
+          Display.score(currentPlayer.name);
       Display.grid.forEach(item => item.removeEventListener("click", turn));
       return currentPlayer.name;
       
@@ -126,7 +145,7 @@ const Game = function(player1, player2) {
 
 
 const player1 = Player("christian", "X")
-const player2 = Player("alex", "O")
+const player2 = Player("niclas", "O")
 let currentGame = Game(player1, player2);
 
 
